8f0a93fc793fe6315244060758f3d66ec116e474
Hamza El-Saawy <hamza.elsaawy@gmail.com>
2016-10-30 22:02:25 -0700

    Finished rebalance
    Added benchmark that rebalances portfolio every so often

--- src/trading/benchmarks.py
+++ src/trading/benchmarks.py
@@ -3,7 +3,8 @@
 Functions to return portfolios using benchmarks
 """
 
-__all__ = ['do_nothing_benchmark']
+__all__ = ['do_nothing_benchmark',
+           'rebalance_benchmark']
 
 import numpy
 import pandas
@@ -51,6 +52,64 @@ def do_nothing_benchmark(stock_a, stock_b, initial_value=1e6,
                                target_weights)
 
     cash = initial_value - na * stock_a.iloc[0] - \
-            nb * stock_b.iloc[0] - 2 * trans_cost
+           nb * stock_b.iloc[0] - 2 * trans_cost
     return make_portfolio(cash, na, nb, na * stock_a + nb * stock_b)
+
+def rebalance_benchmark(stock_a, stock_b, initial_value=1e6, 
+                   target_weights=(0.5, 0.5), trans_cost=0.0,
+                   rebalance_period=5):
+    """Simulates stock perfomance if reabalnced weekly
+
+    Returns portfolio performance that is rebalanced to maintain the original
+    portfolio ratio
+
+    Args:
+    stock_a : pandas.DataFrame
+    stock_b : pandas.DataFrame
+        Dataframes with (ideally) pd.datetime64 indices and closing
+        prices for stocks
+    initial_value : float = 1,000,000
+        Initial cash value of the portfolio
+    target_weights : (float, float) = (0.5, 0.5)
+        Percentage of stocks to be allocated into each stock
+    trans_cost : float = 0
+        transaction cost of buying or selling a stock
+    rebalance_period : integer = 5
+        Length in (stock) days of rebalancing period, starts on the first day
+        of each period. ``1`` means rebalance ever day
+
+    Return:
+    portfolio : portfolio (pandas.DataFrame)
+        A portfolio DataFrame over the same index
+    """
+    _benchmark_validate_args(**locals())
+
+    if (rebalance_period <= 0 or rebalance_period >= len(stock_a)):
+        raise ValueError('rebalance_period must be in (0, time period)')
+
+    rebalance_period = int(rebalance_period)
+    portfolio = make_portfolio(index=stock_a.index)
+
+    total = initial_value
+
+    for k, g in portfolio.groupby(numpy.arange(len(portfolio)) // 
+                                  rebalance_period):
+        index = g.index
+        start = index[0]
+        end = index[-1]
+
+        (na, nb) = allocate_stocks(total, 
+                                   stock_a.loc[start] + trans_cost, 
+                                   stock_b.loc[start] + trans_cost, 
+                                   target_weights)
+        cash = total - na * stock_a.loc[start] - \
+               nb * stock_b.loc[start] - 2 * trans_cost
+
+        portfolio.loc[index, ('A', 'B', 'cash')] = (na, nb, cash)
+        portfolio.loc[index, 'total'] = na * stock_a[index] + \
+                                        nb * stock_b[index] + cash
+
+        total = portfolio.loc[end, 'total']
+
+    return portfolio
 
