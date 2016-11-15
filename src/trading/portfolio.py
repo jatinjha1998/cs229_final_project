e4c3056a557f22334fc9ecd2c5a9c17a26a89c72
Hamza El-Saawy <hamza.elsaawy@gmail.com>
2016-11-14 16:35:14 -0800

    Added stock holding object
    Still working on benchmarks

--- src/trading/portfolio.py
+++ src/trading/portfolio.py
@@ -1,48 +1,112 @@
 """ Portfolio sub-module
 
 A portfolio is a pandas.DataFrame with a time index and the following columns:
-    ca : cost of A's stocks
-    na : number of A's stocks
-    cb : cost of b's stocks
-    nb : number of B's stocks
+    cost_lo : cost of the low-volatility stock
+    num_lo : number of the low-volatility stocks
+    cost_hi : cost of the high-volatility stock
+    num_hi : number of the high-volatility stocks
+    cost_hi : number of B's stocks
     cash : cash assets
     total : total worth of portfolio
 """
 
 __all__ = ['make_portfolio', \
-           'allocate_stocks']
+           'allocate_stocks',\
+           'trade_stocks']
 
 import numpy
 import pandas
 
-def make_portfolio(ca=0, na=0, cb=0, nb=0, cash=0, total=0, index=None):
-    """Makes a portfolia dataframe"""
-    return pandas.DataFrame(data={'ca': ca, 'na': na, 
-                                  'cb': cb, 'nb': nb, 'cash': cash,
-                                  'total': total}, index=index)
+class StockHolding:
+    def __init__(self, cost, num, name=''):
+        self.cost = cost
+        self.num = num
+        self.name = name
 
-def allocate_stocks(total_amount=1E6, 
-                    stock_a=20, stock_b=16, 
+    def __getattr__(self, attr):
+        if attr == 'total':
+            return self.cost * self.num
+
+    def __eq__(self, other):
+        return self.total == other.total
+
+    def __gt__(self, other):
+         return self.total > other.total
+
+    def __add__(self, other):
+        return self.total + other.total
+
+    def __repr__(self):
+        name_str =  '' if (self.name == '')  else self.name + ': '
+        return '{:s}{:d} at {:.2f}$ ({:.2f}$)'.format(name_str,
+               int(self.num), self.cost, self.total)
+
+class PortfolioSnapshot:
+    def __init__(self, lo: StockHolding, hi: StockHolding, cash, total=0):
+        self.lo = lo
+        self.hi = hi
+        self.cash = cash,
+        if total == 0:
+            total = lo + hi + cash
+
+def make_portfolio(cost_lo=0, num_lo=0, cost_hi=0, num_hi=0,
+        cash=0, total=0, index=None):
+    """Makes a portfolia dataframe"""
+    return pandas.DataFrame(data={'cost_lo': cost_lo, 'num_lo': num_lo,
+                                  'cost_lo': cost_hi, 'num_hi': num_hi,
+                                  'cash': cash, 'total': total}, index=index)
+
+
+
+def allocate_stocks(total_amount=1E6,
+                    cost_a=16, cost_b=16, transaction_cost=0.5,
                     target_weights=(0.5, 0.5)):
     """Return number of stocks to buy reach target_weights
 
     Args:
-    stock_a : float
-    stock_b : float
-        Cost of each stock
-    initial_value : float = 1,000,000
+    total_amount: float = 1,000,000
         Initial cash value of the portfolio
+    cost_a : float
+    cost_b : float
+        Cost of each stock
+    detla : float
+        Transaction cost
     target_weights : (float, float) = (0.5, 0.5)
         Percentage of stocks to aim to allocate into each stock
 
     Return:
-    (na, nb) : (int, int)
-        Number of each stock to buy
+    (s1, s2) : (StockHolding, StockHolding)
+    cash: float
     """
-    na = int(numpy.floor(target_weights[0] * total_amount / stock_a))
-    ca = na * stock_a
-    cb = total_amount - ca
-    nb = int(cb // stock_b)
+    na = numpy.floor(target_weights[0] * total_amount / 
+            (cost_a + transaction_cost))
+    s1 = StockHolding(cost_a, na)
+    cash = total_amount - s1.total - na * transaction_cost
+   
+    nb = numpy.floor(cash // (cost_b + transaction_cost))
+    s2 = StockHolding(cost_b, nb)
+    cash -= (s2.total + nb * transaction_cost)
+    return (s1, s2, cash)
 
-    return (na, nb)
+def trade_stocks(percent_trade: float, s1: StockHolding,  s2: StockHolding,
+                 cash: float, transaction_cost: float=0.16, total=0):
+    """Sell off percent_trade * total of the s1 to buy s2"""
+    if not (0 < percent_trade < 1):
+        raise ValueError('percent_trade must be in (0, 1)')
+
+    if total == 0:
+        total = s1 + s2 + cash
+
+    # sell off from the larger stock first
+    delta_s1 = numpy.floor(percent_trade * total / s1.cost)
+    # take into account transaction cost
+    cash += delta_s1 * (s1.cost - transaction_cost)
+    s1.num -= delta_s1
+
+    # buy from smaller stock
+    delta_s2 = numpy.floor(cash / (s2.cost + transaction_cost))
+    cash -= delta_s2 * (s2.cost + transaction_cost)
+    s2.num += delta_s2
+
+    return (s1, s2, cash)
 
