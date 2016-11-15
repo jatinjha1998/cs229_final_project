bc9194066e5a548001655a352656d80d24231fae
Hamza El-Saawy <hamza.elsaawy@gmail.com>
2016-11-14 19:14:49 -0800

    Made sure my benchmark works
    Which is good . . .

--- src/trading/portfolio.py
+++ src/trading/portfolio.py
@@ -5,20 +5,21 @@ A portfolio is a pandas.DataFrame with a time index and the following columns:
     num_lo : number of the low-volatility stocks
     cost_hi : cost of the high-volatility stock
     num_hi : number of the high-volatility stocks
-    cost_hi : number of B's stocks
     cash : cash assets
     total : total worth of portfolio
 """
 
 __all__ = ['make_portfolio', \
            'allocate_stocks',\
-           'trade_stocks']
+           'trade_stocks',\
+           'PortfolioSnapshot',\
+           'StockHolding']
 
 import numpy
 import pandas
 
 class StockHolding:
-    def __init__(self, cost, num, name=''):
+    def __init__(self, cost=0, num=0, name=''):
         self.cost = cost
         self.num = num
         self.name = name
@@ -53,13 +54,13 @@ def make_portfolio(cost_lo=0, num_lo=0, cost_hi=0, num_hi=0,
         cash=0, total=0, index=None):
     """Makes a portfolia dataframe"""
     return pandas.DataFrame(data={'cost_lo': cost_lo, 'num_lo': num_lo,
-                                  'cost_lo': cost_hi, 'num_hi': num_hi,
+                                  'cost_hi': cost_hi, 'num_hi': num_hi,
                                   'cash': cash, 'total': total}, index=index)
 
 
 
 def allocate_stocks(total_amount=1E6,
-                    cost_a=16, cost_b=16, transaction_cost=0.5,
+                    cost_a=16, cost_b=16, trans_cost=0.5,
                     target_weights=(0.5, 0.5)):
     """Return number of stocks to buy reach target_weights
 
@@ -69,7 +70,7 @@ def allocate_stocks(total_amount=1E6,
     cost_a : float
     cost_b : float
         Cost of each stock
-    detla : float
+    trans_cost : float
         Transaction cost
     target_weights : (float, float) = (0.5, 0.5)
         Percentage of stocks to aim to allocate into each stock
@@ -79,33 +80,34 @@ def allocate_stocks(total_amount=1E6,
     cash: float
     """
     na = numpy.floor(target_weights[0] * total_amount / 
-            (cost_a + transaction_cost))
+            (cost_a + trans_cost))
     s1 = StockHolding(cost_a, na)
-    cash = total_amount - s1.total - na * transaction_cost
+    cash = total_amount - s1.total - na * trans_cost
    
-    nb = numpy.floor(cash // (cost_b + transaction_cost))
+    nb = numpy.floor(cash // (cost_b + trans_cost))
     s2 = StockHolding(cost_b, nb)
-    cash -= (s2.total + nb * transaction_cost)
+    cash -= (s2.total + nb * trans_cost)
     return (s1, s2, cash)
 
 def trade_stocks(percent_trade: float, s1: StockHolding,  s2: StockHolding,
-                 cash: float, transaction_cost: float=0.16, total=0):
+                 cash: float, trans_cost: float=0.16, total=0):
     """Sell off percent_trade * total of the s1 to buy s2"""
     if not (0 < percent_trade < 1):
-        raise ValueError('percent_trade must be in (0, 1)')
+        raise ValueError('percent_trade ({}) must be in (0, 1)'.format(percent_trade))
 
     if total == 0:
         total = s1 + s2 + cash
 
     # sell off from the larger stock first
     delta_s1 = numpy.floor(percent_trade * total / s1.cost)
+
     # take into account transaction cost
-    cash += delta_s1 * (s1.cost - transaction_cost)
+    cash += delta_s1 * (s1.cost - trans_cost)
     s1.num -= delta_s1
 
     # buy from smaller stock
-    delta_s2 = numpy.floor(cash / (s2.cost + transaction_cost))
-    cash -= delta_s2 * (s2.cost + transaction_cost)
+    delta_s2 = numpy.floor(cash / (s2.cost + trans_cost))
+    cash -= delta_s2 * (s2.cost + trans_cost)
     s2.num += delta_s2
 
     return (s1, s2, cash)
