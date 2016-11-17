602b6a720813442c9dabfb14dbf7ddebc4e66c4c
Hamza El-Saawy <hamza.elsaawy@gmail.com>
2016-11-17 14:56:03 -0800

    Added incremental Sharpe Ration calculation
    Started working on returns and loss function for Q-learning
    *Started*.

--- src/trading/environment.py
+++ src/trading/environment.py
@@ -7,7 +7,8 @@ __all__ = ['Price', \
            'Shares', \
            'Action', \
            'actions', \
-           'State']
+           'State', \
+           'SharpeRatio']
 
 import numpy as np
 import pandas as pd
@@ -22,9 +23,15 @@ Action = np.float64
 actions = np.array([-0.25, -0.10, -0.05, 0, 0.05, 0.10, 0.25], dtype=Action)
 
 class State:
+    """Defines the agent's state for Q-learning"""
+    @staticmethod
+    def num_states():
+        return 8
+
     def __init__(self, cost_lo_prev: Price, cost_lo: Price, num_lo: Shares,
                 cost_hi_prev: Price, cost_hi: Price, num_hi: Shares,
-                cash: Shares, name_lo: str='', name_hi: str =''):
+                cash: Shares, trans_cost: Price=0,
+                name_lo: str='', name_hi: str =''):
         self.cost_lo_prev = cost_lo_prev
         self.lo = StockHolding(cost_lo, num_lo, name_lo)
 
@@ -32,6 +39,7 @@ class State:
         self.hi = StockHolding(cost_hi, num_hi, name_hi)
 
         self.cash = cash
+        self.trans_cost = trans_cost
 
     def __getattr__(self, attr):
         if attr == 'total':
@@ -42,12 +50,18 @@ class State:
                 self.cash, self.total)
 
     def update(self, cost_lo_new, cost_hi_new):
-        """Update stock costs with a new one"""
+        """Update stock costs with a new one and returns the old total"""
+        old_total = self.total
+
         self.cost_lo_prev, self.lo.cost = self.lo.cost, cost_lo_new
         self.cost_hi_prev, self.hi.cost = self.hi.cost, cost_hi_new
 
+        return old_total
+
     def execute_trade(self, action: Action):
-        """Sell off lo to buy hi (if action > 0, vice-versa if < 0)"""
+        """Sell off lo to buy hi (if action > 0, vice-versa if < 0) Returns old total"""
+        old_total = self.total
+
         if (not action in actions) or (action == 0):
             return
 
@@ -58,5 +72,38 @@ class State:
         percent_trade = np.minimum(sell_stk.total / self.total, abs(action))
 
         (buy_stk, sell_stk, self.cash) = trade_stocks(percent_trade,
-                sell_stk, buy_stk, cash=self.cash, trans_cost=trans_cost)
+                sell_stk, buy_stk, cash=self.cash, trans_cost=self.trans_cost)
+
+        return old_total
+
+
+class SharpeRatio:
+    """Online (incremental) estimation of Sharpe's Ratio
+
+    Uses sample std dev (sₙ), not σₙ
+    Taken from:http://math.stackexchange.com/a/103025
+    """
+
+    def __init__(self, returns: np.array=np.empty(0)):
+        if returns.size == 0:
+            self.mean = 0
+            self.std = 0
+        else:
+            self.mean = np.mean(returns)
+            self.std = np.std(returns)
+        self.n = returns.size
+
+    def update(self, rt: np.float64):
+        old_mean, old_std = self.mean, self.std
+        self.n += 1
+        n = self.n
+
+        if n == 1:
+            self.mean = rt
+            self.std = 0
+        else:
+            self.mean = ( old_mean * (n-1) + rt ) / n
+            self.std = np.sqrt( (n-2)/(n-1)*old_std**2 + (rt - old_mean)**2 / (n) )
+
+        return old_mean, old_std
 
--- src/trading/stock_history.py
+++ src/trading/stock_history.py
@@ -6,6 +6,11 @@ Reads in stock history data. Assumes data only has two columns:
 
 __all__ = ['download_stock_histories', \
            'read_stock_history', \
+           'STOCK_DATA_DIR', \
+           'get_lo_beta_stock_symbols', \
+           'get_hi_beta_stock_symbols', \
+           'LO_BETA_DIR', \
+           'HI_BETA_DIR', \
            'StockPair', \
            'get_stock_pairs']
 
@@ -15,11 +20,11 @@ from os.path import join, splitext, basename, dirname, abspath
 
 import numpy as np
 import pandas as pd
-import pandas_datareader.data
+import pandas_datareader.data as datareader
 from sklearn.model_selection import train_test_split
 
 MODULE_LOC = dirname(abspath(__file__))
-STOCK_DATA_DIR = join(MODULE_LOC, '..', '..', 'data', 'stocks')
+STOCK_DATA_DIR = abspath(join(MODULE_LOC, '..', '..', 'data', 'stocks'))
 HI_BETA_DIR = join(STOCK_DATA_DIR, 'high_beta')
 LO_BETA_DIR = join(STOCK_DATA_DIR, 'low_beta')
 
@@ -58,6 +63,16 @@ def get_stock_pairs(train_size=20):
 
     return train_test_split(stock_pairs, train_size=train_size)
 
+def get_lo_beta_stock_symbols():
+    """List of ticker symbols for low-beta stocks"""
+    return [line.strip() for line in 
+            open(join(LO_BETA_DIR, 'stock_symbols.txt'))]
+
+def get_hi_beta_stock_symbols():
+    """List of ticker symbols for high-beta stocks"""
+    return [line.strip() for line in 
+            open(join(HI_BETA_DIR, 'stock_symbols.txt'))]
+
 def download_stock_histories(path, stock, 
         start_date=DEFAULT_START_DATE, end_date=DEFAULT_END_DATE,
         source='google'):
@@ -84,7 +99,7 @@ def download_stock_histories(path, stock,
     if end_date <= start_date:
         raise ValueError('Start date is not before end date')
 
-    stock_data = pd_datareader.data.DataReader(stock, 
+    stock_data = datareader.DataReader(stock, 
             source, start_date, end_date)
     if isinstance(stock_data, pd.Panel):
         stock_data = stock_data.transpose(2, 1, 0)
