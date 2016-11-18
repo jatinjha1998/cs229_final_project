de1fc9e029c3a62120f747188d27021d566b2d6c
Olivier Jin <ojin@myth6.Stanford.EDU>
2016-11-17 23:27:10 -0800

    Revised minmax with new directory format; added compare script; bugfixes

--- src/trading/minmax.py
+++ src/trading/minmax.py
@@ -12,93 +12,124 @@ def check_dates(date_A, name_A, date_B, name_B):
     return
 
 # ===================================
-# SETUP
+# FUNCTION: Perform min/max, write outputs
 # ===================================
-path = "../../data/"  # Datapath
-cash = 1E3;     # Amt of starting cash
+def minmax(name_A, name_B, opt, \
+   in_path="../../data/", out_path="../../data/", init=1000):
+    
+    # ===================================
+    # SETUP
+    # ===================================
+    
+    # Parsing filenames, creating paths
+    path_A = in_path + name_A
+    if ".csv" not in path_A:
+        path_A = path_A + ".csv"
+
+    path_B = in_path + name_B
+    if ".csv" not in path_B:
+        path_B = path_B + ".csv"
+
+    path_O = out_path + name_A + "_" + name_B + ".csv"
+    name_A = name_A.upper()
+    name_B = name_B.upper()
+
+    opt = False if opt.lower() == "min" else True
+
+    # Opening files
+    try:
+        file_A = open(path_A, 'rb')
+        file_B = open(path_B, 'rb')
+        file_O = open(path_O, 'wb')
+    except IOError:
+        print "ERROR: Can't find input stocks!"
+        exit(1)
+       
+    # Creating readers / writer
+    reader_A = csv.reader(file_A)
+    reader_B = csv.reader(file_B)
+    writer_O = csv.writer(file_O)
+    headers = ['Date', name_A+' Val', name_A+' Amt', name_B+' Val', name_B+' Amt', 'Cash', 'Total']
+    writer_O.writerow(headers)
+
+    # ===================================
+    # ITERATION
+    # ===================================
+    date    = ""    # Date
+    amt_A   = 0     # Amt of stock A to buy
+    amt_B   = 0     # Amt of stock B to buy
+    cash    = 0     # Amt of leftover cash
+    total   = init  # Amt of total value
+
+    # Getting current stocks
+    (cur_date_A, cur_val_A) = reader_A.next()
+    (cur_date_B, cur_val_B) = reader_B.next()
+    cur_val_A = float(cur_val_A)
+    cur_val_B = float(cur_val_B)
+
+    check_dates(cur_date_A, name_A, cur_date_B, name_B)
+
+    while True:
+        date = cur_date_A
+
+        try:
+            # Getting next stocks
+            (nxt_date_A, nxt_val_A) = reader_A.next()
+            (nxt_date_B, nxt_val_B) = reader_B.next()
+            check_dates(nxt_date_A, name_A, nxt_date_B, name_B)
+            nxt_val_A = float(nxt_val_A)
+            nxt_val_B = float(nxt_val_B)
+            amt_A = 0
+            amt_B = 0
+
+            # Comparing growth
+            diff_A = nxt_val_A - cur_val_A
+            amt_A  = math.floor(total / cur_val_A)
+            amt_A  = int(amt_A)
+            diff_A = amt_A * diff_A
+
+            diff_B = nxt_val_B - cur_val_B
+            amt_B  = math.floor(total / cur_val_B)
+            amt_B  = int(amt_B)
+            diff_B = amt_B * diff_B
+
+            if opt != (diff_A < diff_B) :
+                amt_B = 0
+            else:
+                amt_A = 0
+
+            # Writing output
+            cash = total - (amt_A*cur_val_A + amt_B*cur_val_B)
+            cash = round(cash, 2)
+            row = [date, cur_val_A, amt_A, cur_val_B, amt_B, cash, total]
+            writer_O.writerow(row)
+            
+            # Updating values
+            (cur_date_A, cur_val_A) = (nxt_date_A, nxt_val_A)
+            (cur_date_B, cur_val_B) = (nxt_date_B, nxt_val_B)
+            if opt != (diff_A < diff_B) :
+                total = total + diff_A
+                total = round(total, 2)
+            else:
+                total = total + diff_B
+                total = round(total, 2)
+            
+        except StopIteration:
+            break
+
+# ===================================
+# MAIN
+# ===================================
 
 # Validating arguments
 if len(sys.argv) != 3:
     print "Usage: <Stock1> <Stock 2>"
     exit(1)
-
-# Parsing filenames
+    
 name_A = sys.argv[1]
 name_B = sys.argv[2]
-if ".csv" not in name_A:
-    name_A = name_A + ".csv"
-if ".csv" not in name_B:
-    name_B = name_B + ".csv"
-name_O = "out_" + name_A[:-4] + "_" + name_B[:-4] + ".csv"
-
-# Opening files
-try:
-    file_A = open(path+name_A, 'rb')
-    file_B = open(path+name_B, 'rb')
-    file_O = open(path+name_O, 'wb')
-except IOError:
-    print "ERROR: Can't find input stocks!"
-   
-# Creating readers / writer
-reader_A = csv.reader(file_A)
-reader_B = csv.reader(file_B)
-writer_O = csv.writer(file_O)
-writer_O.writerow(['Date', 'Name', 'Amount', 'Value'])
-
-# ===================================
-# ITERATION
-# ===================================
-date = ""    # Date
-name = ""    # Name of stock to buy
-amt  = 0     # Amt of stock to buy
-val  = cash  # Total portfolio value
-
-# Getting current stocks
-(cur_date_A, cur_val_A) = reader_A.next()
-(cur_date_B, cur_val_B) = reader_B.next()
-cur_val_A = float(cur_val_A)
-cur_val_B = float(cur_val_B)
-
-check_dates(cur_date_A, name_A, cur_date_B, name_B)
-
-while True:
-    date = cur_date_A
-
-    try:
-        # Getting next stocks
-        (nxt_date_A, nxt_val_A) = reader_A.next()
-        (nxt_date_B, nxt_val_B) = reader_B.next()
-        check_dates(nxt_date_A, name_A, nxt_date_B, name_B)
-        nxt_val_A = float(nxt_val_A)
-        nxt_val_B = float(nxt_val_B)
-        
-        # Comparing growth
-        diff_A = nxt_val_A - cur_val_A
-        diff_B = nxt_val_B - cur_val_B
-        if diff_A < diff_B :
-            name = name_A
-            amt = math.floor(cash / cur_val_A)
-            amt = int(amt)
-        else:
-            name = name_B
-            amt = math.floor(cash / cur_val_B)
-            amt = int(amt)
-
-        # Writing output
-        writer_O.writerow([date, name[:-4], amt, val])
-        
-        # Updating values
-        (cur_date_A, cur_val_A) = (nxt_date_A, nxt_val_A)
-        (cur_date_B, cur_val_B) = (nxt_date_B, nxt_val_B)
-        if diff_A < diff_B :
-            val = val + amt*diff_A
-            val = round(val, 2)
-        else:
-            val = val + amt*diff_B
-            val = round(val, 2)
-        
-    except StopIteration:
-        break
-
-print "Starting value: " + str(cash)
-print "Ending value: " + str(val)
+    
+for opt in ['min', 'max']:
+    in_dir  = "../../data/"
+    out_dir = "../../data/bench-" + opt + "/"
+    minmax(name_A, name_B, opt, in_dir, out_dir)
